4bf323fd488e1c7323b58ad2c3bb569c82897328
lloy0076 <lloy0076@adam.com.au>
2020-07-12 18:17:54 -0400

    Update lock; change to main.js.

--- src/app.js
+++ src/app.js
@@ -1,48 +1,25 @@
-/**
- *  Copyright 2020 David S. Lloyd.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-import('bootstrap');
-import('bootstrap/dist/css/bootstrap.min.css');
-
-require('gasparesganga-jquery-loading-overlay');
-
 const _ = require('lodash');
-const Swal = require('sweetalert2');
-
-const backbone = require('backbone');
-backbone.emulateHTTP = true;
-backbone.$ = $;
-backbone._ = _;
-
-$.LoadingOverlay('show');
-
 const moment = require('moment');
 const winston = require('winston');
+
+const Swal = require('sweetalert2');
 
 const BookListComponent = require('./BookListComponent');
 const AddComponent = require('./AddComponent');
 const EditComponent = require('./EditComponent');
 
 /**
- * @class App
+ * @class Main
  */
 class App {
     /**
-     * Construct an App class.
+     * Construct an Main class.
      */
-    constructor() {
+    constructor(backbone = null) {
+        if (!backbone) {
+            throw new Error('Cannot start app without a configured backbone instance.');
+        }
+
         this.backbone = backbone;
 
         this.backbone.logger = winston.createLogger({
@@ -58,6 +35,8 @@ class App {
 
         this.setupBookCollection();
         this.setupRoutes();
+
+        this.routeObj = new this.Routes();
     }
 
     /**
@@ -159,9 +138,9 @@ class App {
      * @todo This should be in its own class or service.
      */
     setupBookCollection() {
-        this.Book = backbone.Model.extend({});
+        this.Book = this.backbone.Model.extend({});
 
-        _.extend(this.Book, backbone.Events);
+        _.extend(this.Book, this.backbone.Events);
 
         this.backbone.logger.debug(`Starting at ${moment().format()}.`);
 
@@ -175,40 +154,4 @@ class App {
     }
 }
 
-$(() => {
-    const app = new App(backbone);
-
-    const appRouter = new app.Routes();
-    app.backbone.history.start({
-        pushState: true,
-        route: '/;,',
-    });
-
-    $.LoadingOverlay('hide');
-
-    /**
-     * All navigation that is relative should be passed through the navigate
-     * method, to be processed by the router. If the link has a `data-bypass`
-     * attribute, bypass the delegation completely.
-     */
-    $(document).on("click", "a[href]:not([data-bypass])", function (evt) {
-        // Get the absolute anchor href.
-        const href = { prop: $(this).prop("href"), attr: $(this).attr("href") };
-        // Get the absolute root.
-        const root = location.protocol + "//" + location.host + '/';
-
-        console.error({ href, root });
-
-        // Ensure the root is part of the anchor href, meaning it's relative.
-        if (href.prop.slice(0, root.length) === root) {
-            // Stop the default event to ensure the link will not cause a page
-            // refresh.
-            evt.preventDefault();
-
-            // `Backbone.history.navigate` is sufficient for all Routers and will
-            // trigger the correct events. The Router's internal `navigate` method
-            // calls this anyways.  The fragment is sliced from the root.
-            app.backbone.history.navigate(href.attr, true);
-        }
-    });
-});
+module.exports = App;
